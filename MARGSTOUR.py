ff72090297747386fe24ed6fe67c5f1e03ca9d50
Rayan El-Kotob <rayanelkotob@gmail.com>
2024-12-03 22:40:09 -0500

    added homepage and map, final clean up of styling. last thing needed is to figure out data storing and getting it on a server!

--- MARGSTOUR.py
+++ MARGSTOUR.py
@@ -15,9 +15,69 @@ data = pd.DataFrame(columns=[
     "Margarita Rating",
     "Price Rating",
     "Atmosphere Rating",
-    "Average Rating", 
-    "Comments"
+    "Average Rating"
 ])
+
+@server.route('/home')
+def homepage():
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Margarita Tour</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
+        <style>
+            body {
+                font-family: 'Lilita One', sans-serif;
+                margin: 0;
+                padding: 0;
+                background-color: white;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                height: 100vh;
+                text-align: center;
+            }
+
+            img {
+                max-width: 90%;
+                height: auto;
+                margin-bottom: 20px;
+                border-radius: 8px;
+            }
+
+            .button {
+                width: 90%;
+                max-width: 400px;
+                font-size: 1.2em;
+                color: white;
+                background-color: #abd148;
+                padding: 10px;
+                margin-bottom: 15px;
+                text-decoration: none;
+                border: none;
+                box-sizing: border-box;
+                border-radius: 5px;
+                cursor: pointer;
+                transition: background-color 0.3s ease;
+                text-align: center; /* Center text in the button */
+                display: block; /* Ensure button spans full width */
+            }
+
+            .button:hover {
+                background-color: #86a338;
+            }
+        </style>
+    </head>
+    <body>
+        <img src="static/margs-crawl-home.jpeg" alt="Margarita Tour Image">
+        <a href="/submit" class="button">SUBMIT RATING</a>
+        <a href="/dashboard" class="button">GO TO DASHBOARD</a>
+    </body>
+    </html>
+    """
 
 @server.route('/submit', methods=['GET', 'POST'])
 def submit():
@@ -29,9 +89,10 @@ def submit():
             <head>
                 <title>Margarita Tour Submission</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
                 <style>
                      body {
-                        font-family: Arial, sans-serif;
+                        font-family: 'Lilita One', sans-serif;
                         margin: 0;
                         padding: 0;
                     }
@@ -67,30 +128,34 @@ def submit():
                     }
 
                     button {
-                        background-color: #007bff;
+                        background-color: #abd148;
                         color: white;
                         border: none;
                         font-size: 1.2em;
+                        font-family: 'Lilita One', sans-serif;
                         cursor: pointer;
+                        margin: 5px;
                     }
 
                     button:hover {
-                        background-color: #0056b3;
+                        background-color: #86a338;
                     }
                 </style>
             </head>
             <body>
-                <h1 style="text-align: center;">Submit Your Margarita Ratings</h1>
+                <h1 style="text-align: center;">SUBMIT YOUR MARGS RATING</h1>
                 <form method="post">
-                    <label for="bar">Bar Name:</label>
+                    <label for="bar">BAR NAME:</label>
                     <select id="bar" name="bar" required>
                         <option value="">Select a bar</option>
                         <option value="3 Needs">3 Needs</option>
-                        <option value="Drink">Drink</option>
+                        <option value="Wallflower">Wallflower</option>
+                        <option value="Archives">Archives</option>
                         <option value="Orlandos">Orlandos</option>
+                        <option value="Drink">Drink</option>
                     </select>
                     
-                    <label for="margarita_rating">Margarita Rating (1-10):</label>
+                    <label for="margarita_rating">MARG RATING (1-10):</label>
                     <select id="margarita_rating" name="margarita_rating" required>
                         <option value="">Select a rating</option>
                         <!-- Generate options dynamically -->
@@ -101,7 +166,7 @@ def submit():
                         </script>
                     </select>
 
-                    <label for="price_rating">Price Rating (1-10):</label>
+                    <label for="price_rating">PRICE RATING (1-10):</label>
                     <select id="price_rating" name="price_rating" required>
                         <option value="">Select a rating</option>
                         <script>
@@ -111,7 +176,7 @@ def submit():
                         </script>
                     </select>
 
-                    <label for="atmosphere_rating">Atmosphere Rating (1-10):</label>
+                    <label for="atmosphere_rating">ATMOSPHERE RATING (1-10):</label>
                     <select id="atmosphere_rating" name="atmosphere_rating" required>
                         <option value="">Select a rating</option>
                         <script>
@@ -121,10 +186,9 @@ def submit():
                         </script>
                     </select>
 
-                    <label for="comments">Comments:</label>
-                    <textarea id="comments" name="comments" rows="4"></textarea>
-
-                    <button type="submit">Submit</button>
+                    <button type="submit">SUBMIT</button>
+                    <button type="button" onclick="window.location.href='/dashboard'">GO TO DASHBOARD</button>
+                    <button type="button" onclick="window.location.href='/home'">GO TO HOMEPAGE</button>
                 </form>
             </body>
             </html>
@@ -184,9 +248,10 @@ def submit():
             <head>
                 <title>Thank You!</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
                 <style>
                     body {{
-                        font-family: Arial, sans-serif;
+                        font-family: 'Lilita One', sans-serif;
                         margin: 0;
                         padding: 0;
                         display: flex;
@@ -219,8 +284,10 @@ def submit():
                     button {{
                         width: 100%;
                         font-size: 1.2em;
+                        font-family: 'Lilita One', sans-serif;
+                        margin: 5px auto;
                         padding: 10px;
-                        background-color: #007bff;
+                        background-color: #abd148;
                         color: white;
                         border: none;
                         border-radius: 5px;
@@ -228,38 +295,111 @@ def submit():
                     }}
 
                     button:hover {{
-                        background-color: #0056b3;
+                        background-color: #86a338;
                     }}
                 </style>
             </head>
             <body>
                 <div class="thank-you-container">
-                    <h1>Thank you for your submission!</h1>
-                    <p><strong>Bar:</strong> {bar}</p>
-                    <p><strong>Margarita Rating:</strong> {margarita_rating}</p>
-                    <p><strong>Price Rating:</strong> {price_rating}</p>
-                    <p><strong>Atmosphere Rating:</strong> {atmosphere_rating}</p>
-                    <p><strong>Average Rating:</strong> {average_rating:.2f}</p>
-                    <button onclick="window.location.href='/submit'">Create New Submission</button>
+                    <h1>THANK YOUR FOR YOUR SUBMISSION!</h1>
+                    <p><strong>BAR:</strong> {bar}</p>
+                    <p><strong>MARG RATING:</strong> {margarita_rating}</p>
+                    <p><strong>PRICE RATING:</strong> {price_rating}</p>
+                    <p><strong>ATMOSPHERE RATING:</strong> {atmosphere_rating}</p>
+                    <p><strong>AVERAGE RATING:</strong> {average_rating:.2f}</p>
+                    <button onclick="window.location.href='/submit'">CREATE NEW SUBMISSION</button>
+                    <button type="button" onclick="window.location.href='/dashboard'">GO TO DASHBOARD</button>
+                    <button type="button" onclick="window.location.href='/home'">GO TO HOMEPAGE</button>
                 </div>
             </body>
             </html>
         """
 
-
 # Dash app for the frontend
-app = Dash(__name__, server=server, url_base_pathname='/dashboard/')
+app = Dash(
+    __name__,
+    server=server,
+    url_base_pathname='/dashboard/',
+    external_stylesheets=["https://fonts.googleapis.com/css2?family=Lilita+One&display=swap"]
+)
 
-app.layout = html.Div([
-    html.H1("Margarita Tour Dashboard"),
-    dcc.Graph(id='live-graph'),
-    dcc.Interval(
-        id='interval-component',
-        interval=5*1000,  # Update every 5 seconds
-        n_intervals=0
-    )
-])
+# Define the layout
+app.layout = html.Div(
+    style={
+        "font-family": "'Lilita One', sans-serif",  # Global font
+        "background-color": "white",  # White background
+        "padding": "0",  # No extra padding
+        "margin": "0 auto",  # Center the content
+        "text-align": "center"  # Align all content to the center
+    },
+    children=[
+        html.H1(
+            "MARGS TOUR DASHBOARD",
+            style={
+                "text-align": "center",  # Center the title
+                "margin": "10px 0",  # Minimal vertical spacing
+                "padding": "10px 0"  # Padding around the title
+            }
+        ),
+        # Dynamic graph container
+        html.Div(
+            children=[
+                dcc.Graph(
+                    id='live-graph',
+                ),
+                dcc.Interval(
+                    id='interval-component',
+                    interval=5 * 1000,  # Update every 5 seconds
+                    n_intervals=0
+                )
+            ]
+        ),
+        # Add buttons at the bottom
+         html.Div(
+            style={"margin-top": "20px"},
+            children=[
+                html.A(
+                    "GO TO SUBMISSION PAGE",
+                    href="/submit",
+                    style={
+                        "display": "inline-block",
+                        "background-color": "#abd148",
+                        "color": "white",
+                        "padding": "10px 20px",
+                        "margin": "5px",
+                        "text-decoration": "none",
+                        "border-radius": "5px",
+                        "font-size": "1.2em",
+                        "font-family": "'Lilita One', sans-serif",
+                        "cursor": "pointer",
+                        "transition": "background-color 0.3s ease",
+                        "width": "80%",
+                    }
+                ),
+                html.A(
+                    "GO TO HOMEPAGE",
+                    href="/home",
+                    style={
+                        "display": "inline-block",
+                        "background-color": "#abd148",
+                        "color": "white",
+                        "padding": "10px 20px",
+                        "margin": "5px",
+                        "text-decoration": "none",
+                        "border-radius": "5px",
+                        "font-size": "1.2em",
+                        "font-family": "'Lilita One', sans-serif",
+                        "cursor": "pointer",
+                        "transition": "background-color 0.3s ease",
+                        "width": "80%",
+                    }
+                ),
+            ]
+         )
+    ]
+)
 
+# Callback for updating the graph
 @app.callback(
     Output('live-graph', 'figure'),
     Input('interval-component', 'n_intervals')
@@ -283,10 +423,10 @@ def update_graph(n):
     fig = make_subplots(
         rows=4,
         cols=1,
-        subplot_titles=(
-            "Margarita Ratings", "Price Ratings", "Atmosphere Ratings", "Average Ratings"
-        ),
-        vertical_spacing=0.1
+        subplot_titles=[
+            "MARGS RATINGS", "PRICE RATINGS", "ATMOSPHERE RATINGS", "AVERAGE RATINGS"
+        ],
+        vertical_spacing=0.1  # Spacing between subplots
     )
 
     # Add separate traces for each unique Bar for each rating type
@@ -294,47 +434,21 @@ def update_graph(n):
         filtered_data = data[data['Bar'] == bar]
         color = color_map[bar]
 
-        # Margarita Rating trace
+        # Add traces for different rating types
         fig.add_trace(
-            go.Bar(
-                x=filtered_data['Bar'],
-                y=filtered_data['Margarita Rating'],
-                marker_color=color,
-                name=bar
-            ),
+            go.Bar(x=filtered_data['Bar'], y=filtered_data['Margarita Rating'], marker_color=color, name=bar),
             row=1, col=1
         )
-        
-        # Price Rating trace
         fig.add_trace(
-            go.Bar(
-                x=filtered_data['Bar'],
-                y=filtered_data['Price Rating'],
-                marker_color=color,
-                showlegend=False  # Show the legend only in the first subplot
-            ),
+            go.Bar(x=filtered_data['Bar'], y=filtered_data['Price Rating'], marker_color=color, showlegend=False),
             row=2, col=1
         )
-        
-        # Atmosphere Rating trace
         fig.add_trace(
-            go.Bar(
-                x=filtered_data['Bar'],
-                y=filtered_data['Atmosphere Rating'],
-                marker_color=color,
-                showlegend=False  # Show the legend only in the first subplot
-            ),
+            go.Bar(x=filtered_data['Bar'], y=filtered_data['Atmosphere Rating'], marker_color=color, showlegend=False),
             row=3, col=1
         )
-        
-        # Average Rating trace
         fig.add_trace(
-            go.Bar(
-                x=filtered_data['Bar'],
-                y=filtered_data['Average Rating'],
-                marker_color=color,
-                showlegend=False  # Show the legend only in the first subplot
-            ),
+            go.Bar(x=filtered_data['Bar'], y=filtered_data['Average Rating'], marker_color=color, showlegend=False),
             row=4, col=1
         )
 
@@ -342,19 +456,19 @@ def update_graph(n):
     for i in range(1, 5):  # Iterate through rows 1 to 4
         fig.update_yaxes(
             range=[0, 10],  # Set y-axis range
-            tickmode="linear",  # Show ticks at regular intervals
-            dtick=1,  # Interval between ticks
+            tickmode="linear",  # Linear ticks
+            dtick=1,  # Tick interval
             showgrid=True,  # Show grid lines
-            gridcolor="lightgrey",  # Optional: Set grid line color
+            gridcolor="lightgrey",  # Grid line color
             row=i, col=1
         )
 
-
-    # Update layout for a cleaner look
+    # Update layout for better responsiveness and spacing
     fig.update_layout(
-        showlegend=True,  # Ensure the legend is shown
-        margin=dict(t=50, b=50, r=50),
-        height=1200
+        showlegend=False,
+        margin=dict(t=30, b=30, l=10, r=10),  # Minimal margins for better fit
+        height=1000,  # Adjust height for better display on all devices
+        font={"family": "'Lilita One', sans-serif", "size": 16, "color": "black"}
     )
 
     return fig
